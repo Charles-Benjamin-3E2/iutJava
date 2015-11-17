22f051fe9157b8de78de777fd188befd52e22624
Charles Guitton <cguitt1@ccri-tst04.etu.iut-orsay.fr>
2015-11-17 14:54:28 +0100

    finTP2

--- workspace/iut-tp/src/edu/iut/exceptions/IUTException.java
+++ workspace/iut-tp/src/edu/iut/exceptions/IUTException.java
@@ -1,19 +1,21 @@
 package edu.iut.exceptions;
 
+import edu.iut.app.ApplicationSession;
+
 // Exercice 2 
 public class IUTException extends Exception /* étendre les exceptio, */{
 	public IUTException() {
 		super();
-		// Logger une erreur avec le message empty en utilisant le singleton session, ie le logger défini dans la session */
+		ApplicationSession.instance().getExceptionLogger().warning("Erreur");
 
 	}
 	public IUTException(IUTException e) {
 		super (e);
-		// Logger une erreur avec le message contenu dans 'e'  en utilisant le singleton session, ie le logger défini dans la session */
+		ApplicationSession.instance().getExceptionLogger().warning(getMessage());
 	}
 	public IUTException(String message) {
 		super(message);
-		// Logger une erreur avec le message contenu dans 'message'  en utilisant le singleton session, ie le logger défini dans la session */
+		ApplicationSession.instance().getExceptionLogger().warning(getMessage());
 	}
 	
 }
